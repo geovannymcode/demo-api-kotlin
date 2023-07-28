e3be7c272a123e797feda63441d6456402a44b26
Geovanny Mendoza <geovanny0401@gmail.com>
2023-07-27 22:34:39 -0500

    [V1-0] Initial commit

---
+++ src/test/kotlin/com/guatejug/demo/DemoApiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guatejug.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
